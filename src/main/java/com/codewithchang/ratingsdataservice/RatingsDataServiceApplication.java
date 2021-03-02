3eb56a8bccbe294aadafc297caf26b7819620f26
Chang <cnxiong03@gmail.com>
2021-03-01 22:28:05 -0500

    ratings service

---
+++ src/main/java/com/codewithchang/ratingsdataservice/RatingsDataServiceApplication.java
@@ -0,0 +1,14 @@
+package com.codewithchang.ratingsdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingsDataServiceApplication {
+
+	//Ratings data service stores the grading information
+	public static void main(String[] args) {
+		SpringApplication.run(RatingsDataServiceApplication.class, args);
+	}
+
+}
