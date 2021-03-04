4257883a03bacd8a91b5d61680b06eb3a7a6964b
Chang <cnxiong03@gmail.com>
2021-03-03 23:21:25 -0500

    connected to discovery app

--- src/main/java/com/codewithchang/ratingsdataservice/RatingsDataServiceApplication.java
+++ src/main/java/com/codewithchang/ratingsdataservice/RatingsDataServiceApplication.java
@@ -2,8 +2,10 @@ package com.codewithchang.ratingsdataservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RatingsDataServiceApplication {
 
 	//Ratings data service stores the grading information
